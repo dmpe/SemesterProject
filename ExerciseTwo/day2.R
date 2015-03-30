9f9aedee890c17bbef4f25733bb35a12a994c46b
John Malc <cincenko@seznam.cz>
2015-03-30 15:37:53 +0200

    uplaod first things

--- ExerciseTwo/day2.R
+++ ExerciseTwo/day2.R
@@ -1,22 +1,19 @@
 library(xlsx)
 library(mi)
+library(mice)
+library(Hmisc)
+
+
+datasetM <- read.xlsx("Data/DataSet_01.xlsx", sheetIndex = 1, endRow=277)
+mi.info(datasetM)
+
+typecast(datasetM)
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-m
 
 
 
