f0f5d9d0fcb2f0830c98184f06a20e5a8a175a7c
John Malc <cincenko@seznam.cz>
2015-04-12 15:23:12 +0200

    add correlation

--- ExerciseTwo/day2.R
+++ ExerciseTwo/day2.R
@@ -9,6 +9,8 @@ library(clustrd)
 library("ggthemes")
 library(reshape2)
 library(gridExtra)
+library(corrplot)
+
 
 datasetM <- read.xlsx("Data/DataSet_01.xlsx", sheetIndex = 1, endRow=277)
 mi.info(datasetM)
@@ -100,8 +102,9 @@ table(rota2)
 # Feedback geben konnte? 361, 721, 1080, 1081,
 
 
-
-
+# Correlation
+# http://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+corrplot(cor(datasetM[2:11]), order = "hclust", type = "lower",  method = "number")
 
 
 
