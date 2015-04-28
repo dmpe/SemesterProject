e26ad1cfbbe9a56913e75e107714260e986f4bdc
JohnMalc <f789gh@yahoo.com>
2015-04-28 21:43:50 +0200

    seems to be the best

--- R/Real.R
+++ R/Real.R
@@ -4,6 +4,7 @@ library(stringr)
 library(plyr)
 library(ggplot2)
 library(dplyr)
+library(rvest)
 #####################
 #' https://en.wikipedia.org/wiki/New_York_Supreme_Court,_Appellate_Division
 #' https://stackoverflow.com/questions/26659198/detect-multiple-strings-with-dplyr-and-stringr
@@ -43,43 +44,45 @@ data.NYS <- colwise(function(x){stri_trim_both(x)})(data.NYS)
 # count(johns)
 
 data.NYS$Comp.Name <- tolower(data.NYS$Comp.Name)
-stringsToCheck <- c("corporation.", "corp", "corp.", "group", "pllc", "p.a.", "llp","l.p.", "lp", "llc", "l.l.c.", "l.l.p.",
-                    "ltd.","inc.", "inc", "p.c.", "pc", "plc")
-patTTT <- paste(stringsToCheck, collapse = '|')
 testingString <- as.character(na.omit(data.NYS$Comp.Name))
 
+stringsToCheck <- c("corporation.", "corporation", "corp", "corp.", "group", "gmbh", "company", "pllc", "llp", "llc", "l.l.c.", "l.l.p.",
+                    "ltd.","inc.", "inc", "plc", "p.c.", "pc", "p.a.", "l.p.", "lp", "co.", "l.p")
+patTTT <- paste(stringsToCheck, collapse = '|')
 
-# zatim nefung "[&,]"
-checkAndCleanFormatting <- function(x) {
-  x <- stri_trim_both(x)  
-  # print(length(x))
-  for(i in 1:length(x)) {
-    if(stri_detect_regex(x[i],patTTT) == TRUE) {
-      # print("yes, the string does contain some of them")
-      x[i] <- str_replace(x[i], patTTT, "")
-    }
-  }
-  x <- stri_trim_both(x)
-}
+
+# # zatim nefung "[&,]"
+# checkAndCleanFormatting <- function(x) {
+#   x <- stri_trim_both(x)  
+#   # print(length(x))
+#   for(i in 1:length(x)) {
+#     if(stri_detect_regex(x[i],patTTT) == TRUE) {
+#       # print("yes, the string does contain some of them")
+#       x[i] <- str_replace(x[i], patTTT, "")
+#     }
+#   }
+#   x <- stri_trim_both(x)
+# }
 
 # testingString <- checkAndCleanFormatting(testingString)
 # testingStringSmall <- testingString[1:100000]
-# for(i in 1:3) {
-data.NYS$Comp.Name <- sapply(as.character(data.NYS$Comp.Name), function(x) {
-  if ( is.na(x) == T) {
-    x <- NA
-  } else {
-    x <- stri_replace_first_fixed(x, "&", "and")
-    x <- stri_replace_last_regex(x, ",", "")
-    if(stri_detect_regex(x, patTTT) == TRUE) {
-      x <- str_replace(x, patTTT, "") 
+for(i in 1:2) {
+  data.NYS$Comp.Name <- sapply(as.character(data.NYS$Comp.Name), function(x) {
+    if ( is.na(x) == T) {
+      x <- NA
+    } else {
+      x <- stri_replace_first_fixed(x, "&", "and")
+      x <- stri_replace_last_fixed(x, "and", "")
+      x <- stri_replace_last_regex(x, ",", "") 
+      if(stri_detect_regex(x, patTTT) == TRUE) {
+        x <- str_replace(x, patTTT, "") 
+      }
+      x <- stri_trim_both(x)
     }
-    x <- stri_trim_both(x)
-  }
-})
-# }
+  })
+}
 
-saddawda <- data.frame(testingString)
+# saddawda <- data.frame(testingString)
 
 
 
@@ -89,31 +92,49 @@ saddawda <- data.frame(testingString)
 # }) 
 
 # data.NYS$Comp.Name <- str_replace_all(data.NYS$Comp.Name, "[&,]", "")
-# 
-# com_name <- data.frame(table(str_trim(data.NYS$Comp.Name)))
-# com_name <- plyr::arrange(com_name, desc(com_name$Freq))
-# 
+
+com_name <- data.frame(table(str_trim(data.NYS$Comp.Name)))
+com_name <- plyr::arrange(com_name, desc(com_name$Freq))
+com_name$Var1 <- as.character(com_name$Var1)
+
 # com_name.plot <- ggplot(data=com_name[1:20,], aes(x=reorder(Var1, Freq), y=Freq)) + coord_flip(ylim= c(400,830))
 # com_name.plot <- com_name.plot + geom_bar(stat = "identity") + theme(axis.text.y = element_text(size = 14), axis.text.x = element_text(size = 14)) # angle = 90, vjust = 0.5, hjust=1, 
 # com_name.plot <- com_name.plot + ylab("How many registered lawyers do companies have in NYC ?") + xlab("Companies")
 # com_name.plot <- com_name.plot + scale_y_continuous(breaks = seq(400, 830, 50))
 # com_name.plot
-# 
-# 
-# 
+
+
+
 f500 <- html("http://www.zyxware.com/articles/4344/list-of-fortune-500-companies-and-their-websites") %>%
   html_node(".data-table") %>%
   html_table(header = TRUE)
-f500 <- f500[1:100, ]
+# f500 <- f500[1:100, ]
 
 f500.com <- data.frame(Names = tolower(f500$Company))
+# f500.com$Names <- str_replace_all(f500.com$Names, "inc", "")
+# f500.com$Names <- str_replace_all(f500.com$Names, "inc.", "")
+# f500.com$Names <- stri_replace_last_fixed(f500.com$Names, "l.p.", "")
+# f500.com$Names <- str_replace_all(f500.com$Names, "[.,]", "")
+# f500.com$Names <- str_replace_all(f500.com$Names, "&", "and")
 
-f500.com$Names <- str_replace_all(f500.com$Names, "inc", "")
-f500.com$Names <- str_replace_all(f500.com$Names, "inc.", "")
-f500.com$Names <- str_replace_all(f500.com$Names, "l.p.", "")
-f500.com$Names <- str_replace_all(f500.com$Names, "[.,]", "")
+
+# for(i in 1:2) {
+f500.com$Names <- sapply(as.character(f500.com$Names), function(x) {
+  if ( is.na(x) == T) {
+    x <- NA
+  } else {
+    x <- stri_replace_first_fixed(x, "&", "and")
+    x <- stri_replace_last_fixed(x, "and", "")
+    if(stri_detect_regex(x, patTTT) == TRUE) {
+      x <- stri_replace_last_regex(x, patTTT, "") 
+    }
+    x <- stri_replace_last_fixed(x, ",", "")
+    x <- stri_replace_last_fixed(x, ".", "")
+    x <- stri_trim_both(x)
+  }
+})
+# }
+
+right.join <- right_join(com_name, f500.com, by = c("Var1" = "Names"))
 
 
-# com_name$Var1 <- as.character(com_name$Var1)
-# rightj <- right_join(com_name, f500.com, by = c("Var1" = "Names"))
-# 
