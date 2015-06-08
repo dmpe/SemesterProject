8a66ed217e1109f2e5319871e1ccefe325a19e9c
dmpe <cincenko@seznam.cz>
2015-06-08 14:55:45 +0200

    update to stop displaying pics

--- R/day4.R
+++ R/day4.R
@@ -5,8 +5,8 @@ library(clustrd)
 library(reshape2)
 library(corrplot)
 library(car)
-library(dplyr)
 library(plyr)
+library(dplyr)
 
 set.seed(5157)
 
@@ -22,13 +22,13 @@ dataset.product$User.Engage
 dataset.product.correlation <- dataset.product[, !names(dataset.product) %in% c("Duration", "Post.ID", "TotalReach", "ProductDataSet")]
 
 product <- cor(dataset.product.correlation)
-corrplot(product, type="lower", method="number")
+# corrplot(product, type="lower", method="number")
 
-dev.off()
-par(mfrow = c(1,2))
-corrplot(product, order = "hclust", type="lower", method="number")
-corrplot(corrrePart, order = "hclust", type = "lower",  method = "number")
-dev.off()
+# dev.off()
+# par(mfrow = c(1,2))
+# corrplot(product, order = "hclust", type="lower", method="number")
+# corrplot(corrrePart, order = "hclust", type = "lower",  method = "number")
+# dev.off()
 
 ##################
 # Joined data Set
@@ -47,15 +47,15 @@ joinedDataSets.without$Gender[joinedDataSets.without$Gender==1] <- "Man"
 joinedDataSets.without$TypeDataSet[joinedDataSets.without$TypeDataSet==0] <- "Service"
 joinedDataSets.without$TypeDataSet[joinedDataSets.without$TypeDataSet==1] <- "Produkt"
 
-sp <- ggplot(joinedDataSets.without, aes(x=Likes, y=User.Engage)) + geom_point(shape=1) + stat_smooth(method = "lm")
-sp <- sp + facet_grid(TypeDataSet ~ Gender)
-sp
-
-
-sp <- ggplot(joinedDataSets.without, aes(x=Likes, y=User.Engage)) + geom_point(shape=1) + stat_smooth(method = "lm")
-sp <- sp + facet_grid(Experience ~ Gender)
-sp
-
+# sp <- ggplot(joinedDataSets.without, aes(x=Likes, y=User.Engage)) + geom_point(shape=1) + stat_smooth(method = "lm")
+# sp <- sp + facet_grid(TypeDataSet ~ Gender)
+# sp
+# 
+# 
+# sp <- ggplot(joinedDataSets.without, aes(x=Likes, y=User.Engage)) + geom_point(shape=1) + stat_smooth(method = "lm")
+# sp <- sp + facet_grid(Experience ~ Gender)
+# sp
+# 
 
 
 
@@ -69,72 +69,72 @@ rozdil <- as.data.frame(corrrePart) - as.data.frame(product)
 # sweep(corrrePart,2,c(0,0,product))
 
 # https://stackoverflow.com/questions/4357031/qqnorm-and-qqline-in-ggplot2
-qqplot.data <- function (vec) # argument: vector of numbers
-{
-  # following four lines from base R's qqline()
-  y <- quantile(vec[!is.na(vec)], c(0.25, 0.75))
-  x <- qnorm(c(0.25, 0.75))
-  slope <- diff(y)/diff(x)
-  int <- y[1L] - slope * x[1L]
-  
-  d <- data.frame(resids = vec)
-  
-  ggplot(d, aes(sample = resids)) + stat_qq() + geom_abline(slope = slope, intercept = int)
-  
-}
-
-qqplot.data(datasetM$User.Engage)
-qqplot.data(datasetM$Acquaintance)
-qqplot.data(datasetM$Experience)
-
-qqplot.data(dataset.product$User.Engage)
-qqplot.data(dataset.product$Acquaintance)
-qqplot.data(dataset.product$Experience)
-
-
-
-## User Engage
-qqnorm(joinedDataSets.without$User.Engage)
-qqline(joinedDataSets.without$User.Engage)
-
-
-qqnorm((datasetM$User.Engage^(-1)-1)/-1)
-qqline((datasetM$User.Engage^(-1)-1)/-1)
-
-cor(qqnorm((joinedDataSets.without$User.Engage^1-1)/1)$x, qqnorm((joinedDataSets.without$User.Engage^1-1)/1)$y)
-
-cor(qqnorm((joinedDataSets.without$User.Engage^2-1)/2)$x, qqnorm((joinedDataSets.without$User.Engage^2-1)/2)$y)
-cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
-cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
-cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
-
-qqnorm((datasetM$User.Engage^2-1)/2)
-qqline((datasetM$User.Engage^2-1)/2)
-
-qqnorm((datasetM$User.Engage^3-1)/3)
-qqline((datasetM$User.Engage^3-1)/3)
-
-qqnorm((datasetM$User.Engage^4-1)/4)
-qqline((datasetM$User.Engage^4-1)/4)
-
-
-qqnorm(datasetM$Acquaintance)
-qqline(datasetM$Acquaintance)
-
-qqnorm((datasetM$Acquaintance^2-1)/2)
-qqline((datasetM$Acquaintance^2-1)/2)
-
-qqnorm((datasetM$Acquaintance^3-1)/3)
-qqline((datasetM$Acquaintance^3-1)/3)
-
-qqnorm((datasetM$Acquaintance^4-1)/4)
-qqline((datasetM$Acquaintance^4-1)/4)
-
-
-qqnorm(log(datasetM$Acquaintance))
-qqline(log(datasetM$Acquaintance))
-
-
+# qqplot.data <- function (vec) # argument: vector of numbers
+# {
+#   # following four lines from base R's qqline()
+#   y <- quantile(vec[!is.na(vec)], c(0.25, 0.75))
+#   x <- qnorm(c(0.25, 0.75))
+#   slope <- diff(y)/diff(x)
+#   int <- y[1L] - slope * x[1L]
+#   
+#   d <- data.frame(resids = vec)
+#   
+#   ggplot(d, aes(sample = resids)) + stat_qq() + geom_abline(slope = slope, intercept = int)
+#   
+# }
+# 
+# qqplot.data(datasetM$User.Engage)
+# qqplot.data(datasetM$Acquaintance)
+# qqplot.data(datasetM$Experience)
+# 
+# qqplot.data(dataset.product$User.Engage)
+# qqplot.data(dataset.product$Acquaintance)
+# qqplot.data(dataset.product$Experience)
+# 
+# 
+# 
+# ## User Engage
+# qqnorm(joinedDataSets.without$User.Engage)
+# qqline(joinedDataSets.without$User.Engage)
+# 
+# 
+# qqnorm((datasetM$User.Engage^(-1)-1)/-1)
+# qqline((datasetM$User.Engage^(-1)-1)/-1)
+# 
+# cor(qqnorm((joinedDataSets.without$User.Engage^1-1)/1)$x, qqnorm((joinedDataSets.without$User.Engage^1-1)/1)$y)
+# 
+# cor(qqnorm((joinedDataSets.without$User.Engage^2-1)/2)$x, qqnorm((joinedDataSets.without$User.Engage^2-1)/2)$y)
+# cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
+# cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
+# cor(qqnorm((datasetM$User.Engage^1-1)/1)$x, qqnorm((datasetM$User.Engage^1-1)/1)$y)
+# 
+# qqnorm((datasetM$User.Engage^2-1)/2)
+# qqline((datasetM$User.Engage^2-1)/2)
+# 
+# qqnorm((datasetM$User.Engage^3-1)/3)
+# qqline((datasetM$User.Engage^3-1)/3)
+# 
+# qqnorm((datasetM$User.Engage^4-1)/4)
+# qqline((datasetM$User.Engage^4-1)/4)
+# 
+# 
+# qqnorm(datasetM$Acquaintance)
+# qqline(datasetM$Acquaintance)
+# 
+# qqnorm((datasetM$Acquaintance^2-1)/2)
+# qqline((datasetM$Acquaintance^2-1)/2)
+# 
+# qqnorm((datasetM$Acquaintance^3-1)/3)
+# qqline((datasetM$Acquaintance^3-1)/3)
+# 
+# qqnorm((datasetM$Acquaintance^4-1)/4)
+# qqline((datasetM$Acquaintance^4-1)/4)
+# 
+# 
+# qqnorm(log(datasetM$Acquaintance))
+# qqline(log(datasetM$Acquaintance))
+# 
+# 
 
 
 
