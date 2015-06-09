62bad4a152070d7e8990299faee938cd234c8278
dmpe <cincenko@seznam.cz>
2015-06-09 15:56:27 +0200

    update PP

---
+++ R/day6.R
@@ -0,0 +1,29 @@
+mat  <- matrix(1, length(xAQ2), 2)
+eigens <- eigen(ellip2)
+ellip2 <- t(ellip) %*% ellip
+
+evs <- sqrt(eigens$values)
+evecs <- eigens$vectors
+
+a <- evs[1]
+b <- evs[2]
+x0 <- 0
+y0 <- 0
+alpha <- atan(evecs[ , 1][2] / evecs[ , 1][1])
+theta <- seq(0, 2 * pi, length=(1000))
+
+x <- x0 + a * cos(theta) * cos(alpha) - b * sin(theta) * sin(alpha)
+y <- y0 + a * cos(theta) * sin(alpha) + b * sin(theta) * cos(alpha)
+
+
+
+
+plot(x, y, type = "l", main = expression("x = a cos " * theta * " + " * x[0] * " and y = b sin " * theta * " + " * y[0]), asp = 1)
+arrows(0, 0, a * evecs[ , 1][2], a * evecs[ , 1][2])
+arrows(0, 0, b * evecs[ , 2][3], b * evecs[ , 2][2])
+
+
+
+ellipse(c(0, 0), shape=ellip2, radius=0.98, col="red", lty=2)
+
+
